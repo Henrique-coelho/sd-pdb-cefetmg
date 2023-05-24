e23253eaa13925bcde23e1e29cf8420eede368b3
Pablo Henrique Santos <pablohsgc@gmail.com>
2023-05-24 09:56:28 -0300

    Adição de módulo básico de tolerância a falhas

---
+++ tolerancia/moduloTolerancia.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+$script=$*
+
+# data e horario do sistema 
+data=`date +'%Y-%m-%d|%H:%M'`
+
+./insertfault_tolerancy.sh 1 INICIADO $data 111 $1 
+
+# executa tarefa e exporta resultado
+$script > statusTask.txt
+
+lineErro=`cat statusTask.txt | grep Erro | wc -l`
+
+# data e horario apos o fim do processamento
+data=`date +'%Y-%m-%d|%H:%M'`
+
+if [ $lineErro -gt 0 ]; then
+./updateTask.sh 1 PENDENTE $data 111 $1
+# aqui deve ser alguma estrutura para repetir a tarefa n vezes
+else
+./updateTask.sh 1 CONCLUIDO $data 111 $1
+fi
