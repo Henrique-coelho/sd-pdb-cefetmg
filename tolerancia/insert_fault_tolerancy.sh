bcbe7ef2cf9ce3497a440ab85b24cf9ab5d31758
Pablo Henrique Santos <pablohsgc@gmail.com>
2023-05-24 09:55:52 -0300

    Update insert_fault_tolerancy.sh
    Correção de inserção

--- tolerancia/insert_fault_tolerancy.sh
+++ tolerancia/insert_fault_tolerancy.sh
@@ -10,9 +10,10 @@ COLUMN1_VALUE="$1"
 COLUMN2_VALUE="$2"
 COLUMN3_VALUE="$3"
 COLUMN4_VALUE="$4"
+COLUMN5_VALUE="$5"
 
 insert_into_mysql() {
-    local query="INSERT INTO $TABLE_NAME (Task_Status, Last_Modification, System_ID, Task_Name) VALUES ('$COLUMN1_VALUE', '$COLUMN2_VALUE', '$COLUMN3_VALUE','$COLUMN4_VALUE');"
+    local query="INSERT INTO $TABLE_NAME (interactions_id, task_Status, last_modification, system_id, task_name) VALUES ($COLUMN1_VALUE, '$COLUMN2_VALUE', '$COLUMN3_VALUE',$COLUMN4_VALUE,'$COLUMN5_VALUE');"
     mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" -e "$query"
 }
 
