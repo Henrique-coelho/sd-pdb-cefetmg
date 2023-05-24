f8c5c334100f2d82945a416698942b349aa5e96b
Alexandre Roque <61557947+alexandre-roque@users.noreply.github.com>
2023-05-24 10:04:50 -0300

    Update verifyFolder.sh

--- naming/verifyFolder.sh
+++ naming/verifyFolder.sh
@@ -7,13 +7,13 @@ path=$1
 quantity=$(find "$path" -maxdepth 1 -type f | wc -l)
 
 # verifica se a quantity de arquivos é menor que 1 milhão
-if [ "$quantity" -lt 1000000 ] && [ "$(find "$path/$new_name" -maxdepth 1 -type f | wc -l)" -lt 1000000 ]; then
+if [ "$quantity" -lt 1000000 ]; then
   echo "file_name"
 else
   number=1
   while true; do
     new_name="file_name_$number"
-    if [ ! -e "$path/$new_name" ]; then
+    if [ ! -e "$path/$new_name" ] && [ "$(find "$path/$new_name" -maxdepth 1 -type f | wc -l)" -lt 1000000 ]; then
       echo "$new_name"
       break
     fi
