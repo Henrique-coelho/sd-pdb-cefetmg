5288e2f43e1fff817330989060c13c713b2e4785
Ronaldo Mendonça Zica <ronaldomendoncazica@gmail.com>
2023-05-24 10:11:27 -0300

    tradução nos comentários (padronização)

--- naming/extractFolderName.sh
+++ naming/extractFolderName.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-# Check if the correct number of command-line arguments is provided
+# Verifica se a quantidade correta de parâmetros foi informada
 if [ $# -ne 4 ]; then
     echo "Usage: $0 <variable1> <variable2> <variable3> <variable4>"
     exit 1
 fi
 
-# Set the file_path to "interacoes.csv"
+# Arquivo com interações
 file_path="interacoes.csv"
 
-# Check if the file exists
+# Arquivo pode não existir
 if [ -f "$file_path" ]; then
-    # Read the file and search for the matching line
+    # Lê o arquivo e encontra a linha correspondente
     while IFS=: read -r var1 var2 var3 var4 rest; do
-        # Check if the variables match the provided arguments
+        # Todas as variáveis batem com os parâmetros
         if [[ "$var1" == *"$1"* && "$var2" == *"$2"* && "$var3" == *"$3"* && "$var4" == *"$4"* ]]; then
-            # Extract the information after the fourth colon ":"
-            information=${rest#*:*:*:*:}
+            # Extrai a informação após o quatro :
+            interation_name=${rest#*:*:*:*:}
 
-            # Print the information
-            echo "$information"
+            # Imprime o nome da interação
+            echo "$interation_name"
             exit 0
         fi
     done < "$file_path"
 
-    # If no matching line is found
     echo "No matching interaction found for the given variables."
 else
     echo "The file $file_path does not exist."
