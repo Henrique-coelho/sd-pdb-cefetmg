3b9d1599f1fc813a42c5c6018adfa50c44d20093
Ronaldo Mendonça Zica <ronaldomendoncazica@gmail.com>
2023-05-24 09:33:46 -0300

    Update extractFolderName.sh

--- naming/extractFolderName.sh
+++ naming/extractFolderName.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 
-# Check if file_path is provided as a command-line argument
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <file_path>"
+# Check if the correct number of command-line arguments is provided
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 <variable1> <variable2> <variable3> <variable4>"
     exit 1
 fi
 
-# Retrieve the file_path from command-line argument
-file_path=$1
+# Set the file_path to "interacoes.csv"
+file_path="interacoes.csv"
 
 # Check if the file exists
 if [ -f "$file_path" ]; then
-    # Read the first line of the file
-    first_line=$(head -n 1 "$file_path")
+    # Read the file and search for the matching line
+    while IFS=: read -r var1 var2 var3 var4 rest; do
+        # Check if the variables match the provided arguments
+        if [[ "$var1" == *"$1"* && "$var2" == *"$2"* && "$var3" == *"$3"* && "$var4" == *"$4"* ]]; then
+            # Extract the information after the fourth colon ":"
+            information=${rest#*:*:*:*:}
 
-    # Split the first line into parts based on whitespace
-    parts=($first_line)
+            # Print the information
+            echo "$information"
+            exit 0
+        fi
+    done < "$file_path"
 
-    # Check if there are enough elements
-    if [ ${#parts[@]} -ge 2 ]; then
-        # Get the first and second element
-        atm1=${parts[0]}
-        resn1=${parts[1]}
-
-        # Concatenate the elements to form the folder name
-        folder_name="$atm1:$resn1"
-
-        # Use the folder name as needed
-        echo "$folder_name"
-    else
-        echo "The first line of the file does not have enough elements."
-    fi
+    # If no matching line is found
+    echo "No matching interaction found for the given variables."
 else
     echo "The file $file_path does not exist."
 fi
