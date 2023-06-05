56cc2dc8eb30adfb70bac7e1123baae4a3271090
Ronaldo Mendonça Zica <ronaldomendoncazica@gmail.com>
2023-06-05 09:57:30 -0300

    correções na extração

--- naming/extractFolderName.sh
+++ naming/extractFolderName.sh
@@ -12,11 +12,11 @@ file_path="../interacoes.csv"
 # Arquivo pode não existir
 if [ -f "$file_path" ]; then
     # Lê o arquivo e encontra a linha correspondente
-    while IFS=: read -r var1 var2 var3 var4 rest; do
+    while IFS=: read -r var1 var2 var3 var4; do
         # Todas as variáveis batem com os parâmetros
-        if [[ "$var1" == *"$1"* && "$var2" == *"$2"* && "$var3" == *"$3"* && "$var4" == *"$4"* ]]; then
+        if [[ "$var1" == "$1 $2" || "$var2" == "$3 $4" ]]; then
             # Extrai a informação após o quarto :
-            interation_name=${rest#*:*:*:*:}
+            interation_name=$var4
 
             # Imprime o nome da interação
             echo "$interation_name"
@@ -24,7 +24,7 @@ if [ -f "$file_path" ]; then
         fi
     done < "$file_path"
 
-    echo "No matching interaction found for the given variables."
+    echo "Erro - Tipo de ligação não identificado."
 else
-    echo "The file $file_path does not exist."
+    echo "Erro - Não foi possível localizar $file_path."
 fi
