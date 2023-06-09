a475fc2e1267c0e8da01af8c8efe1488d2e25665
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2023-06-09 10:53:38 -0300

    merge optimizedHunter and residueHunter

--- processing/residueHunter.sh
+++ processing/residueHunter.sh
@@ -1,81 +1,90 @@
-usage ()
+#!/bin/bash
+
+usage()
 {
-  echo "Usage : Script <pdb_file_path> <res1> <atm1> <res2> <atm2> <cutoff>";
-  exit;
+    echo "Usage : $0 <pdb_file_path> <res1> <atm1> <res2> <atm2> <cutoff>";
+    exit;
 }
 
 if [ "$#" -ne 6 ];
 then
-  usage;
+    usage;
 fi
 
 pids=()
-max_processos=5 # procurar time de arquitura para definimos qual sera o maximo de instancias possiveis
+max_processes=5 # Architeture team must define max number for process
 
-executar_processo() {
-  ./distanceCalculatorFileGenerator.sh $pdb $res1 $atm1 $resn1 $res2 $atm2 $resn2 $cutoff
-}
+# boolean
+false=0;
+true=1;
 
 # file path
-pdb=$1
+pdb=$1;
 
 # information about interactor 1
-res1=$2
-atm1=$3
+res1=$2;
+atm1=$3;
 
 # information about interactor 2
-res2=$4
-atm2=$5
+res2=$4;
+atm2=$5;
 
-# candidates optimized form
-# for resn1 in `egrep "^ATOM *[0-9]+ *$atm1 *$res1 *A" $1 | awk '{ print substr($0,23,4) }'`; do 
-#   for resn2 in `egrep "^ATOM *[0-9]+ *$atm2 *$res2 *A" $1 | awk '{ print substr($0,23,4) }'`; do 
-#     ./distanceCalculatorFileGenerator.sh $pdb $res1 $atm1 $resn1 $res2 $atm2 $resn2 $cutoff &
-#   done;
-# done;
+# cutoff distance
+cutoff=$6;
 
+execute_distance_calculator()
+{
+    # parameters passed as $# in local scope
+    ./distanceCalculator.sh $1 $2 $3 $4 $5 $6 $7 $8
+}
 
-# verificar forma de tentar deixar a execucao em paralelo, para melhorar a perfomance do codigo
-# resn1 candidates
-vet_resn1=()
-for resn1 in `egrep "^ATOM *[0-9]+ *$atm1 *$res1 *A" $1 | awk '{ print substr($0,23,4) }'`; do
-  #verifica se o valor nao e nulo para adicionar o valor no vetor
-  if [ ! -z "$resn1" ];
-  then
-    vet_resn1+=($resn1);
-  fi
+there_is_resn2=$false;
+there_is_interaction=$false;
+for resn1 in `egrep "^ATOM *[0-9]+ *$atm1 *$res1 *A" $pdb | awk '{ print substr($0,23,4) }'`; do
+    # in the exemple, there are 24 SER OG in file
+    # if $atm1 $res1 not found, the script will not loop second for
+    
+    for resn2 in `egrep "^ATOM *[0-9]+ *$atm2 *$res2 *A" $pdb | awk '{ print substr($0,23,4) }'`;do
+        # in the exemple, so the script must look for LYS NZ 24 times
+        # there is no residue elimination, one of then can match with many others
+        
+        # Execute distance calculator
+        execute_distance_calculator $pdb $res1 $atm1 $resn1 $res2 $atm2 $resn2 $cutoff &
+        pids+=($!)
+        
+        # While pids for equals or greater then the maximum allowed process, wait to call another one
+        while [ ${#pids[@]} -ge $max_processes ]; do
+            wait
+        done
+
+        # if $atm2 $res2 found, script must continue first for
+        there_is_resn2=$true;
+
+        ############# to do: count generated tail files;
+        there_is_interaction=$true; 
+    done;
+    if [ "$there_is_resn2" -eq "$false" ]; 
+    then
+        # there is no resn2 applicants, then first for must stop
+        break;
+    fi
+done;
+
+# Wait for any remaining processes to finish
+for pid in "${pids[@]}"; do
+    wait "$pid"
 done
 
-#verifica se tem valor no primeiro vetor para executar a verificacao de res2
-if [ ${#vet_resn1[@]} -gt 0 ];
+if [ "$there_is_interaction" -eq "$true" ];
 then
-  # resn2 candidates
-  vet_resn2=()
-  for resn2 in `egrep "^ATOM *[0-9]+ *$atm2 *$res2 *A" $1 | awk '{ print substr($0,23,4) }'`; do
-    #verifica se o valor nao e nulo para adicionar o valor no vetor
-    if [ ! -z "$resn2" ];
-    then
-      vet_resn2+=($resn2);
-    fi
-  done
+    echo "ZIP";
+    # if enters here, means that there is at least candidates
+    # there is the empty directory problem
 
-  #verifica se tem dado no vetor de resn2 para poder realizar o calculo de distancia
-  if [ ${#vet_resn2[@]} -gt 0 ]; then
-  # find distance and generate valid files
-  for resn1 in "${vet_resn1[@]}"; do
-    for resn2 in "${vet_resn2[@]}"; do
-      executar_processo &
-      pids+=($!)
-      # enquanto o numero de pids for igual ou maior que o maximo de processos permitidos, ele vai aguardar ate que algum processo termine, para que possa incluir um novo processo
-      while [ ${#pids[@]} -ge max_processos ]; do
-        wait
-      done
-    done
-  done
+    ################################
+    # this part needs improvements #
+    ################################
 
-  # Wait for any remaining processes to finish ( aqui é para quando sair do for, para garantir que todos os pids terminaram de executar antes de continuar o codigo )
-  for pid in "${pids[@]}"; do
-    wait "$pid"
-  done
-  fi
+    # zip the entire path
+    # zip -r "$path.zip" "$path";
 fi
