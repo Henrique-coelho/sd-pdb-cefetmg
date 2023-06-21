07178025d29b41e4f739033043c68dcfe628b7d8
Pablo Henrique Santos <pablohsgc@gmail.com>
2023-06-21 10:30:50 -0300

    Update tolerancy_module.sh

--- tolerancia/tolerancy_module.sh
+++ tolerancia/tolerancy_module.sh
@@ -3,21 +3,59 @@
 script=$*
 
 # data e horario do sistema 
-data=`date +'%Y-%m-%d|%H:%M'`
+data=`date +'%Y-%m-%d|%H:%M'`;
 
-./insertfault_tolerancy.sh 1 INICIADO $data 111 $1 
+# extrai id do dispositivo
+system_id=$(hostname -I);
 
-# executa tarefa e exporta resultado
-$script 2> statusTask.txt
+# extrai um nome para o script
+nome_script=`echo $@ | sed 's/ /_/g'`;
+nome_saida=$nome_script"_out.txt";
+nome_status=$nome_script"_status.txt";
 
-lineErro=`cat statusTask.txt | wc -l`
+executa_script(){
+	# executa tarefa e exporta resultado
+	$script > $nome_saida 2> $nome_status
 
-# data e horario apos o fim do processamento
-data=`date +'%Y-%m-%d|%H:%M'`
+	# soma erros, da saida de erro e saida padrao
+	erros=`cat $nome_status | wc -l`;
+	erros_saida_padrao=`cat $nome_saida | grep -e Erro -e Usage | wc -l`;
+	qtd_erros=`echo "$erros + $erros_saida_padrao" | bc`;
 
-if [ $lineErro -gt 0 ]; then
-./updateTask.sh 1 PENDENTE $data 111 $1
-# aqui deve ser alguma estrutura para repetir a tarefa n vezes
+	# data e horario apos o fim do processamento
+	data=`date +'%Y-%m-%d|%H:%M'`
+}
+
+# insere tarefa na base de dados
+./insertTask.sh 1 INICIADO $data $system_id $nome_script 
+
+# primeira execucao do script
+executa_script
+
+if [ $qtd_erros -gt 0 ]; then
+./updateTask.sh 1 PENDENTE $data $system_id $nome_script
+
+	CONTADOR=0;
+
+	while [ $CONTADOR -lt 3 ]; do
+		executa_script
+
+		if [ $qtd_erros -eq 0 ]; then
+			./updateTask.sh 1 CONCLUIDO $data $system_id $nome_script
+			break
+		if
+
+		./updateTask.sh 1 PENDENTE $data $system_id $nome_script
+
+		let CONTADOR=CONTADOR+1; 
+	done
+
 else
-./updateTask.sh 1 CONCLUIDO $data 111 $1
+
+./updateTask.sh 1 CONCLUIDO $data $system_id $nome_script
+
 fi
+
+#remove arquivos de saida criados
+rm $nomeSaida
+rm $nomeStatus
