53fcc71f925b9885cfcfd29a7b6d581c1e82cf6f
laraloures <lara.loures2@gmail.com>
2023-06-21 21:10:06 -0300

    change parallel (teacher's idea)

--- processing/residueHunter.sh
+++ processing/residueHunter.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-usage()
-{
+usage() {
     echo "Usage : $0 <pdb_file_path> <res1> <atm1> <res2> <atm2> <cutoff>";
     exit;
 }
 
-if [ "$#" -ne 6 ];
-then
+if [ "$#" -ne 6 ]; then
     usage;
 fi
 
-pids=()
 max_processes=5 # Architeture team must define max number for process
 
 # boolean
@@ -32,80 +29,62 @@ atm2=$5;
 # cutoff distance
 cutoff=$6;
 
-execute_distance_calculator()
-{
+execute_distance_calculator() {
     # parameters passed as $# in local scope
     # include tolerance
-    ./distanceCalculator.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
+    ./distanceCalculator.sh "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}"
 }
 
 pdb_without_extension="${pdb%.ent}";
-root_path=${pdb_without_extension};
+root_path="${pdb_without_extension}";
 mkdir -p "$root_path";
 
 there_is_resn2=$is_false;
 there_is_interaction=$is_false;
-for resn1 in `egrep "^ATOM *[0-9]+ *$atm1 *$res1 *A" $pdb | awk '{ print substr($0,23,4) }'`; do
-    # in the exemple, there are 24 SER OG in file
+for resn1 in $(egrep "^ATOM *[0-9]+ *$atm1 *$res1 *A" "$pdb" | awk '{ print substr($0,23,4) }'); do
+    # in the example, there are 24 SER OG in file
     # if $atm1 $res1 not found, the script will not loop second for
     
-    for resn2 in `egrep "^ATOM *[0-9]+ *$atm2 *$res2 *A" $pdb | awk '{ print substr($0,23,4) }'`;do
-        # in the exemple, so the script must look for LYS NZ 24 times
-        # there is no residue elimination, one of then can match with many others
+    for resn2 in $(egrep "^ATOM *[0-9]+ *$atm2 *$res2 *A" "$pdb" | awk '{ print substr($0,23,4) }'); do
+        # in the example, so the script must look for LYS NZ 24 times
+        # there is no residue elimination, one of them can match with many others
         
-        type_of_interaction=`../naming/extractFolderName.sh "$res1" "$atm1" "$res2" "$atm2"`;
+        type_of_interaction=$(../naming/extractFolderName.sh "$res1" "$atm1" "$res2" "$atm2");
         path="$root_path/$type_of_interaction";
 
         # Create the directory if it doesn't exist
         mkdir -p "$path";
         
         # Execute distance calculator
-        execute_distance_calculator $pdb $res1 $atm1 $resn1 $res2 $atm2 $resn2 $cutoff $path $pdb_without_extension &
-        pids+=($!)
+        execute_distance_calculator "$pdb" "$res1" "$atm1" "$resn1" "$res2" "$atm2" "$resn2" "$cutoff" "$path" "$pdb_without_extension" &
         
-        # While pids for equals or greater then the maximum allowed process, wait to call another one
-        while [ ${#pids[@]} -ge $max_processes ]; do
-            wait -n # para aguardar qualquer processo que esta sendo executado em segundo plano seja executado
-            for pid in "${pids[@]}"; do # para ir em cada um dos pids e descobrir qual pode ser excluido (no bash, o wait retorna o codigo de saida mas nao o PID finalizado, por isso precisamos percorrer a lista para descobrir qual é)
-                if ! kill -0 "$pid" 2>/dev/null; then # O comando kill -0 "$pid" verifica se o processo existe, mas não envia nenhum sinal ao processo. Se o processo não existir mais, o comando kill retornará um status de saída diferente de zero, indicando que o processo foi encerrado.
-                    # O comando 2>/dev/null é para direcionar qualquer erro que possa ser dado para o "buraco negro" /dev/null, onde so irá ser descartado e não mostrará no console a mensagem de erro
-                    pids=("${pids[@]/$pid}") # Remove o PID do processo concluído do array pids
-                fi
-            done
-            pids=("${pids[@]}") # Remove os elementos vazios do array pids
+        # While the number of running processes is equal to or greater than the maximum allowed processes, wait before starting another one
+        while [ "$(ps -ef | grep 'distanceCalculator' | wc -l)" -ge "$max_processes" ]; do
+            sleep 1 #é para ajudar a evitar um loop infinito consumindo recursos da CPU enquanto espera pelos processos, tipo um intervalo para reduzir um pouco do uso da CPU constante
         done
-
+        
         # if $atm2 $res2 found, script must continue first for
         there_is_resn2=$is_true;
- 
-    done;
-    if [ "$there_is_resn2" -eq "$is_false" ]; 
-    then
+    done
+    
+    if [ "$there_is_resn2" -eq "$is_false" ]; then
         # there is no resn2 applicants, then first for must stop
         break;
     fi
-done;
+done
 
 # Wait for any remaining processes to finish
-while [ ${#pids[@]} -gt 0 ]; do
-    wait -n 
-    for pid in "${pids[@]}"; do
-        if ! kill -0 "$pid" 2>/dev/null; then 
-            # Remove o PID do processo concluído do array pids
-            pids=("${pids[@]/$pid}")
-        fi
-    done
-    pids=("${pids[@]}") # Remove os elementos vazios do array pids
+while [ "$(ps -ef | grep 'distanceCalculator' | wc -l)" -gt 1 ]; do
+    sleep 1
 done
 
 # find at least one generated file;
-there_is_interaction=$(du -s $root_path | awk '{ print $1 }');
-if [ "$there_is_interaction" -gt "4" ];
-then 
-  # if enters here, means that there is at least candidates
-  # zip the entire path
-  zip -r "$root_path.zip" "$root_path";
-  # Dúvida: Nesse momento já sabemos as informações de conexão com o servidor?
-  # ../replication/dump.sh create localhost "MYSQL_USERNAME" "MYSQL_PASSWORD" "DATABASE_NAME";
-  # ../replication/send.sh;
+there_is_interaction=$(du -s "$root_path" | awk '{ print $1 }');
+if [ "$there_is_interaction" -gt 4 ]; then 
+    # if enters here, means that there is at least candidates
+    # zip the entire path
+    zip -r "$root_path.zip" "$root_path";
+    # Dúvida: Nesse momento já sabemos as informações de conexão com o servidor?
+    # ../replication/dump.sh create localhost "MYSQL_USERNAME" "MYSQL_PASSWORD" "DATABASE_NAME";
+    # ../replication/send.sh;
 fi
